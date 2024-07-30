7bd8970e3f1122190dd223638de59e45bf75af36
Nick Wolff <nick@wolff.tech>
2024-07-30 05:03:09 -0400

    change resource names to "this"
    - Changing names of resources to "this" for simplcity.

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
-resource "azurerm_resource_group" "resource_group_tc" {
+resource "azurerm_resource_group" "this" {
   name     = "Wolff-RG-${var.prefix}"
   location = var.location
   tags     = var.tags
 }
 
-resource "azurerm_virtual_network" "virtual_network_tc" {
+resource "azurerm_virtual_network" "this" {
   name                = "Wolff-VN-${var.prefix}"
-  resource_group_name = azurerm_resource_group.resource_group_tc.name
-  location            = azurerm_resource_group.resource_group_tc.location
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
   address_space       = var.address_space
   tags                = var.tags
 }
 
-resource "azurerm_subnet" "subnet-tc" {
+resource "azurerm_subnet" "this" {
   for_each             = var.subnet_map
   name                 = "Wolff-Subnet_${each.key}-${var.prefix}"
-  resource_group_name  = azurerm_resource_group.resource_group_tc.name
-  virtual_network_name = azurerm_virtual_network.virtual_network_tc.name
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = each.value
 }
 
-resource "azurerm_network_security_group" "network_security_group-tc" {
+resource "azurerm_network_security_group" "this" {
   name                = "Wolff-NSG-${var.prefix}"
-  location            = azurerm_resource_group.resource_group_tc.location
-  resource_group_name = azurerm_resource_group.resource_group_tc.name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
 }
 
-resource "azurerm_subnet_network_security_group_association" "subnet_network_security_group_association-tc" {
-  subnet_id                 = azurerm_subnet.subnet-tc["Web"].id
-  network_security_group_id = azurerm_network_security_group.network_security_group-tc.id
+resource "azurerm_subnet_network_security_group_association" "this" {
+  subnet_id                 = azurerm_subnet.this["Web"].id
+  network_security_group_id = azurerm_network_security_group.this.id
 }
