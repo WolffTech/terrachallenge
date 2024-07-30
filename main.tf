030bc593b0a50e996584598685fe3af9b6940319
Nick Wolff <nick@wolff.tech>
2024-07-30 04:18:12 -0400

    create the network security group and associate it

--- main.tf
+++ main.tf
@@ -19,3 +19,14 @@ resource "azurerm_subnet" "subnet-tc" {
   virtual_network_name = azurerm_virtual_network.virtual_network_tc.name
   address_prefixes     = each.value
 }
+
+resource "azurerm_network_security_group" "network_security_group-tc" {
+  name                = "Wolff-NSG-${var.prefix}"
+  location            = azurerm_resource_group.resource_group_tc.location
+  resource_group_name = azurerm_resource_group.resource_group_tc.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_network_security_group_association-tc" {
+  subnet_id                 = azurerm_subnet.subnet-tc["Web"].id
+  network_security_group_id = azurerm_network_security_group.network_security_group-tc.id
+}
