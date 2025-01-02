6eb7e62bb7cc53e92c6cd9b0e3252977405c723c
Nick Wolff <nick@wolff.tech>
2025-01-02 14:21:47 -0500

    Create Key Vault module

---
+++ modules/keyvault/main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_key_vault" "kv" {
+  name                        = var.key_vault_name
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  enabled_for_disk_encryption = var.enabled_for_disk_encryption
+  tenant_id                   = var.tenant_id
+  soft_delete_retention_days  = var.soft_delete_retention_days
+  purge_protection_enabled    = var.purge_protection_enabled
+  sku_name                    = var.sku_name
+
+  access_policy {
+    tenant_id = var.tenant_id
+    object_id = var.object_id
+
+    key_permissions = [
+      "Get", "List", "Create", "Delete",
+    ]
+
+    secret_permissions = [
+      "Get", "List", "Set", "Delete",
+    ]
+
+    certificate_permissions = [
+      "Get", "List", "Create", "Delete",
+    ]
+  }
+
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+    ip_rules       = var.allowed_ip_ranges
+  }
+}
+
