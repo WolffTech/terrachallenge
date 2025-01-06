f3c9f3191606853aa4e138a6393c798b877fa7fd
Nick Wolff <nick@wolff.tech>
2025-01-06 11:20:41 -0500

    cleanup: Switch to this in rg module
    Switching to using "this" for the resource name in the rg module. This
    is normally best practice when creating modules to use "this" for the
    name of the resource for easy reference.

--- modules/rg/main.tf
+++ modules/rg/main.tf
@@ -3,7 +3,7 @@ locals {
   all_tags     = merge(local.default_tags, var.az_tags)
 }
 
-resource "azurerm_resource_group" "az-rg" {
+resource "azurerm_resource_group" "this" {
   name     = var.az_rg_name
   location = var.az_rg_location
 
--- modules/rg/outputs.tf
+++ modules/rg/outputs.tf
@@ -1,7 +1,7 @@
 output "az-rg-name" {
-  value = azurerm_resource_group.az-rg.name
+  value = azurerm_resource_group.this.name
 }
 
 output "az-rg-location" {
-  value = azurerm_resource_group.az-rg.location
+  value = azurerm_resource_group.this.location
 }
