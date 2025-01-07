7413371f7e828f50c94f5eda38ac7015fef6b280
Nick Wolff <nick@wolff.tech>
2025-01-07 08:13:06 -0500

    Add security rule to allow HTTP
    Need this to allow HTTP connections to external connections can hit
    NGINX on the linux boxes through the load balancer.

--- main.tf
+++ main.tf
@@ -52,6 +52,18 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 130
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsga" {
