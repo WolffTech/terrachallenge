dca7a487831ec134aaeadb2bca01e027bc1b30d5
Nick Wolff <nick@wolff.tech>
2025-01-07 07:23:51 -0500

    load_balancer: add variables to use for health probe

--- variables.tf
+++ variables.tf
@@ -70,3 +70,28 @@ variable "pass_min_numeric" {
   type        = number
   default     = 2
 }
+
+variable "probe_protocol" {
+  type    = string
+  default = "Http"
+}
+
+variable "probe_port" {
+  type    = number
+  default = 80
+}
+
+variable "probe_interval" {
+  type    = number
+  default = 15
+}
+
+variable "probe_unhealthy_threshold" {
+  type    = number
+  default = 2
+}
+
+variable "probe_request_path" {
+  type    = string
+  default = "/"
+}
